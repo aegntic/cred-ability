547788fb77ddd7e0d3201f4b374a8be3c7ebb0a7
SEO Engineering <admin@seo.engineering>
2025-04-24 14:14:49 +1000

    Deploy smaller files to gh-pages

---
+++ assets/features-animation-xOWyUW8p.js
@@ -0,0 +1,10 @@
+import { b7 as gestureAnimations, e as animations, g as createDomVisualElement } from "./index-BU_7202x.js";
+const domAnimation = {
+  renderer: createDomVisualElement,
+  ...animations,
+  ...gestureAnimations
+};
+export {
+  domAnimation as d
+};
+//# sourceMappingURL=features-animation-xOWyUW8p.js.map
---
+++ assets/src-UW24ZMRV-CHqSNnWU.js
@@ -0,0 +1,7 @@
+import { d as domAnimation } from "./features-animation-xOWyUW8p.js";
+import "./index-BU_7202x.js";
+var src_default = domAnimation;
+export {
+  src_default as default
+};
+//# sourceMappingURL=src-UW24ZMRV-CHqSNnWU.js.map
